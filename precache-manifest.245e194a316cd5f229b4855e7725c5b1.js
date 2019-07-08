c41fadcde8234d583e38dfe12228236615ac1455
MR.GHOST <bouzouitayassine@gmail.com>
2019-07-08 13:17:06 +0200

    Updates

---
+++ precache-manifest.245e194a316cd5f229b4855e7725c5b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187708e1b7f9ec38c86bdeff95c13232",
+    "url": "/RiddlesBot-LeaderBoard/index.html"
+  },
+  {
+    "revision": "aa55a6a94a79e0e92f9c",
+    "url": "/RiddlesBot-LeaderBoard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1676c9902c84096df2c1",
+    "url": "/RiddlesBot-LeaderBoard/static/js/2.61d87809.chunk.js"
+  },
+  {
+    "revision": "aa55a6a94a79e0e92f9c",
+    "url": "/RiddlesBot-LeaderBoard/static/js/main.9ec257d2.chunk.js"
+  },
+  {
+    "revision": "03d29377cde3e0d28faa",
+    "url": "/RiddlesBot-LeaderBoard/static/js/runtime~main.6800f155.js"
+  }
+]);
